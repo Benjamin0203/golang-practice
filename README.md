69fc30d4f361cf7e62831699785cac942c198a82
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-03-17 23:34:38 -0700

    --add content to README.md

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 ---
 
 [Return and handle an error](https://go.dev/doc/tutorial/handle-errors): import `errors` package and return error in `greetings.go` file. In `hello.go` file we handle the error.
+
+---
+
+[Return random greeting](https://go.dev/doc/tutorial/random-greeting): import `math/rand` package and use `rand.Intn` function to return random greeting. In `hello.go` file we call `greetings.Hello` function with random greeting by running `go run .` command in terminal.
--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
     "fmt"
     "errors"
+    "math/rand"
+    "time"
 )
 
 // Hello returns a greeting for the named person.
@@ -11,6 +13,26 @@ func Hello(name string) (string, error) {
         return "", errors.New("empty name")
     }
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    // message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+//init sets initial values for variables used in the function.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+// randomFormat returns one of a set of greeting messages.
+//randomFormat starts with a lowercase letter, making it accessible only to code in its own package (in other words, it's not exported).
+
+// The returned message is selected at random.
+func randomFormat() string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+    // Return one of the message formats selected at random.
+    return formats[rand.Intn(len(formats))]
 }
